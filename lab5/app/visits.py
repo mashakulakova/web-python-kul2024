7dba12d9ad411714636b5d2bc61c1d7b767c0c8b
masha kulakova <mashakulakova04@inbox.ru>
2024-06-12 00:10:17 +0300

    lab5_csv

--- lab5/app/visits.py
+++ lab5/app/visits.py
@@ -123,28 +123,77 @@ def show_user():
     return render_template('/visits/show_user.html', values=values, count=count, page=page)
 
 
-@bp_visit.route('/send_csv')
-def send_csv():
-    querry = '''
-    SELECT * FROM visit_logs
-    '''
+@bp_visit.route('/send_csv_visits')
+def send_csv_visits():
+    query = 'SELECT * FROM visit_logs'
     cursor = db.connection().cursor(named_tuple=True)
-    cursor.execute(querry)
+    cursor.execute(query)
     records = cursor.fetchall()
     csv_text = ''
     for record in records:
+        csv_text += 'Number: '
         csv_text += str(record.id)
-        csv_text += ', '
+        csv_text += '; '
+        csv_text += 'Path: '
         csv_text += str(record.path)
-        csv_text += ', '
+        csv_text += '; '
+        csv_text += 'User: '
         csv_text += str(record.user_id)
-        csv_text += ', '
+        csv_text += '; '
+        csv_text += 'Created: '
         csv_text += str(record.created_at)
+        csv_text += '; '
+        csv_text += '\n'
+    cursor.close()
+    mem = io.BytesIO()
+    mem.write(csv_text.encode())
+    mem.seek(0)
+    return send_file(mem, as_attachment=True, download_name='csv_text.csv')
+
+@bp_visit.route('/send_csv_pages')
+def send_csv_pages():
+    query = 'SELECT path, COUNT(user_id) AS count_path FROM visit_logs GROUP BY path ORDER BY count_path DESC'
+    cursor = db.connection().cursor(named_tuple=True)
+    cursor.execute(query)
+    records = cursor.fetchall()
+    csv_text = ''
+    for record in records:
+        csv_text += 'Path: '
+        csv_text += str(record.path)
+        csv_text += '; '
+        csv_text += 'Count of visits: '
+        csv_text += str(record.count_path)
+        csv_text += '; '
+        csv_text += '\n'
+    cursor.close()
+    mem = io.BytesIO()
+    mem.write(csv_text.encode())
+    mem.seek(0)
+    return send_file(mem, as_attachment=True, download_name='csv_pages.csv')
+
+@bp_visit.route('/send_csv_users')
+def send_csv_users():
+    query = '''
+        SELECT IFNULL(CONCAT(users.last_name, ' ', users.first_name,' ', users.middle_name), 'Неаутентифицированный пользователь') AS fio, 
+        count(*) AS cnt 
+        FROM visit_logs 
+        LEFT JOIN users ON visit_logs.user_id = users.id 
+        GROUP BY fio ORDER BY cnt DESC
+        '''
+    cursor = db.connection().cursor(named_tuple=True)
+    cursor.execute(query)
+    records = cursor.fetchall()
+    csv_text = ''
+    for record in records:
+        csv_text += 'User: '
+        csv_text += str(record.fio)
+        csv_text += ', '
+        csv_text += 'Count: '
+        csv_text += str(record.cnt)
         csv_text += ', '
         csv_text += '\n'
     cursor.close()
     mem = io.BytesIO()
     mem.write(csv_text.encode())
     mem.seek(0)
-
-    return send_file(mem, as_attachment=True, download_name='csv_text.csv')
+    return send_file(mem, as_attachment=True, download_name='csv_users.csv')
